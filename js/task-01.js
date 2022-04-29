80b9d1bf49bafc90bc4f4592bb182d61a676ed3c
Nataliia Hrynchuk <92783918+NataliiaHrynchuk@users.noreply.github.com>
2022-04-29 20:43:35 +0300

    add task 1,2,3,,5,6

--- js/task-01.js
+++ js/task-01.js
@@ -1,21 +1,45 @@
-const parent = document.querySelector("#parent");
-const child = document.querySelector("#child");
-const descendant = document.querySelector("#descendant");
+const refs ={
+   parent: document.querySelector("#parent"),
+   child: document.querySelector("#child"),
+   descendant: document.querySelector("#descendant"),
+}
 
-parent.addEventListener("click", () => {
-  alert(
-    "Батьківський обробник кліків. Це сповіщення не з’явиться при натисканні на Нащадок, подія не потрапить сюди!"
-  );
-});
+// refs.parent.addEventListener("click", () => {
+//   alert(
+//     "Батьківський обробник кліків. Це сповіщення не з’явиться при натисканні на Нащадок, подія не потрапить сюди!"
+//   );
+// });
+refs.parent.addEventListener("click", onParentClick);
 
-child.addEventListener("click", () => {
-  alert(
-    "Обробник дочірніх кліків. Це сповіщення не з’явиться при натисканні на Нащадок, подія не потрапить сюди!"
-  );
-});
+// refs.child.addEventListener("click", () => {
+//   alert(
+//     "Обробник дочірніх кліків. Це сповіщення не з’явиться при натисканні на Нащадок, подія не потрапить сюди!"
+//   );
+// });
 
-descendant.addEventListener("click", (event) => {
-  event.stopPropagation();
+refs.child.addEventListener("click", onChildClick);
 
-  alert("Нащадковий обробник кліків");
-});
+// refs.descendant.addEventListener("click", (event) => {
+//   event.stopPropagation();
+//   alert("Нащадковий обробник кліків");
+// });
+
+refs.descendant.addEventListener("click", oneDescendantClick);
+
+function onParentClick(evt) {
+  console.log('onParentClick');
+  console.log('onParentClick -> evt.target', evt.target);
+  console.log('onParentClick -> evt.currentTarget', evt.currentTarget);
+}
+
+function onChildClick(evt) {
+  console.log('onChildClick');
+  console.log('onChildClick -> evt.target', evt.target);
+  console.log('onChildClick -> evt.currentTarget', evt.currentTarget);
+}
+
+function oneDescendantClick(evt) {
+  console.log('oneDescendantClick');
+  console.log('oneDescendantClick -> evt.target', evt.target);
+  console.log('oneDescendantClick -> evt.currentTarget', evt.currentTarget);
+}
---
+++ js/task-03.js
@@ -0,0 +1,7 @@
+const output = document.querySelector(".output");
+let scrollEventCounter = 0;
+
+document.addEventListener("scroll", () => {
+  scrollEventCounter += 1;
+  output.textContent = scrollEventCounter;
+});
